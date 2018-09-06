740cab749f8035d8bb60c561e0564a9ed3c43a8c
Cheyin L <cheyinl@gmail.com>
2018-09-07 02:41:16 +0800

    add setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+""" Setup script for Angular Host-page Template Backend and Utility for Django """
+
+from setuptools import setup
+
+setup(
+		name="django-angular-host-page-template-backend",
+		version="0.0.1",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
+		description="Angular Host-page Template Backend and Utility for Django",
+		packages=[
+				"angularhostpagetemplate",
+		])
+
+# vim: ts=4 sw=4 ai nowrap
