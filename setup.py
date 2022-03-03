9c95ebedce5b0db81f85c952ed8ecd4bc27e86eb
Cheyin L <cheyinl@gmail.com>
2022-03-04 01:42:05 +0800

    add classifier and license

--- setup.py
+++ setup.py
@@ -10,6 +10,15 @@ setup(
 		packages=[
 				"angularhostpagetemplate",
 				"angularhostpagetemplate.tool",
-		])
+		],
+		classifiers=[
+				'Development Status :: 5 - Production/Stable',
+				'Intended Audience :: Developers',
+				'License :: OSI Approved :: MIT License',
+				'Programming Language :: Python :: 2.7',
+				'Framework :: Django :: 1.11',
+		],
+		license='MIT License',
+)
 
 # vim: ts=4 sw=4 ai nowrap
